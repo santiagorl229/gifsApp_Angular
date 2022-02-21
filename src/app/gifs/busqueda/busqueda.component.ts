9a6bb7b6673d6725628648e84e4e8ac4e3b9578d
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-02-21 16:44:48 -0500

    Se termina el video 10 del modulo 7

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,9 +10,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class BusquedaComponent {  
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService){}
+
   buscar(){
     const valor= this.txtBuscar.nativeElement.value;
-    console.log(valor)
+    if(valor.trim().length==0){
+      return;
+    }
+    this.gifsService.buscarGifs(valor);
     this.txtBuscar.nativeElement.value="";
   }
 
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SidebarComponent {
+  get historial(){
+    return this.gifsService.historial;    
   }
+
+  constructor(private gifsService: GifsService) { }
+
 
 }
