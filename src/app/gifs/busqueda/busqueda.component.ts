305285ae7ca4819edb3d1a9015f01ecf980a68c8
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-02-21 13:07:36 -0500

    Se termina el video 8 del modulo 7 los viewChild. Funcionan para hacer referencia del html y asignarle una funcionalidad a los eventos en typeScript, en el ejemplo es con los input

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {  
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buscar(){
+    const valor= this.txtBuscar.nativeElement.value;
+    console.log(valor)
+    this.txtBuscar.nativeElement.value="";
   }
 
 }
