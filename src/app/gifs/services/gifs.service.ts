8f93f4701a5f4c3ae3d366790d3e0d8289274328
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-03-04 17:11:29 -0500

    Iniciar el video 15 del modulo 7 Gifs de la parte 2 de 5

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
   private apiKey: string = 'KfzLshRKQyjXUeBxa6jp0HzdwhRsvSHB';
   private _historial: string[]= [];
-  public resultados: any[]=[];
+  public resultados: Gif[]=[];
 
   get historial(){
     return [...this._historial];
@@ -22,8 +23,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=KfzLshRKQyjXUeBxa6jp0HzdwhRsvSHB&q=${query}}&limit=10`)
-             .subscribe((resp: any)=>{
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=KfzLshRKQyjXUeBxa6jp0HzdwhRsvSHB&q=${query}}&limit=10`)
+             .subscribe((resp)=>{
                  console.log(resp.data);
                  this.resultados =resp.data;
               });
