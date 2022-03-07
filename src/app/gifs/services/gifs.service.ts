186492df63a656d2fb779e6c6ab004f287e70607
Santiago Rodriguez Lopez <srodriguezl@sistecredito.com>
2022-03-07 16:30:52 -0500

    Estoy en el video 18 minuto 1:46 del modulo 7

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
@@ -14,19 +14,26 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!)|| [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!)|| [];
+  }
 
   buscarGifs(query: string=''){
     query = query.trim().toLocaleLowerCase();
     if(!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
+
+    const params = new HttpParams()
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=KfzLshRKQyjXUeBxa6jp0HzdwhRsvSHB&q=${query}}&limit=10`)
              .subscribe((resp)=>{
                  console.log(resp.data);
                  this.resultados =resp.data;
+                 localStorage.setItem('resultados', JSON.stringify(this.resultados));
               });
 
       // this.http.get('https://postman-echo.com/delay/2')
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -12,5 +12,7 @@ export class SidebarComponent {
 
   constructor(private gifsService: GifsService) { }
 
-
+  buscar(termino: string){
+    this.gifsService.buscarGifs(termino);
+  }
 }
